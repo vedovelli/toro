1a9c45d98c49b468ce7bf2207de1166f74d4c9ff
Vedovelli <vedovelli@gmail.com>
2014-03-31 20:24:53 -0300

    Primeira implementacao do login com facebook.

--- js/application.js
+++ js/application.js
@@ -5,3 +5,26 @@ window.Toro = Ember.Application.create();
 /*Config info*/
 Toro.configs ={};
 Toro.configs.webservice_uri = 'http://toro.ved:4730';
+Toro.configs.root_uri = 'http://toro.ved/';
+
+// From https://github.com/jaredhanson/passport-facebook/issues/12#issuecomment-5913711
+// Remove the ugly Facebook appended hash
+// <https://github.com/jaredhanson/passport-facebook/issues/12>
+if (window.location.hash && window.location.hash === "#_=_") {
+  // If you are not using Modernizr, then the alternative is:
+  //   `if (window.history && history.pushState) {`
+  if (Modernizr.history) {
+    window.history.pushState("", document.title, window.location.pathname);
+  } else {
+    // Prevent scrolling by storing the page's current scroll offset
+    var scroll = {
+      top: document.body.scrollTop,
+      left: document.body.scrollLeft
+    };
+    window.location.hash = "";
+    // Restore the scroll offset, should be flicker free
+    document.body.scrollTop = scroll.top;
+    document.body.scrollLeft = scroll.left;
+  }
+}
+
--- nodejs/app.js
+++ nodejs/app.js
@@ -1,14 +1,7 @@
 
-/*App setup & init*/
-var express = require('express');
-var app = express();
-app.listen(4730);
-
-
-
-
 /*Database setup, init & models*/
 var Candidato;
+var Usuario;
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://127.0.0.1/toro');
 var db = mongoose.connection;
@@ -26,11 +19,105 @@ db.once('open', function callback () {
 		}
 	});
 
+	var usuarioSchema = mongoose.Schema({
+		id: Number,
+		displayName: String,
+		gender: String,
+		provider: String,
+		hometown: String,
+		location: String
+	});
+
 	Candidato = mongoose.model('Candidato', candidatoSchema);
+	Usuario = mongoose.model('Usuario', usuarioSchema);
 
 });
 
 
+/*Passport (authentication module)*/
+var passport = require('passport');
+var FacebookStrategy = require('passport-facebook').Strategy;
+
+
+passport.use(new FacebookStrategy({
+		clientID: '1433705530208473',
+		clientSecret: '77dbcee96747a99e24a78806dcebca2b',
+		callbackURL: "http://toro.ved:4730/auth/facebook/callback"
+	},
+	function(accessToken, refreshToken, profile, done) {
+		var usuario;
+		Usuario.find({id: profile.id}, function(err, usuarios) {
+			if (err) { return done(err); }
+			if (usuarios.length === 0) {
+				usuario = new Usuario({
+					id: profile.id,
+					displayName: profile.displayName,
+					gender: profile.gender,
+					provider: profile.provider,
+					hometown: profile._json.hometown,
+					location: profile._json.location
+				});
+			} else {
+				usuario = usuarios[0];
+				usuario.id = profile.id,
+				usuario.displayName = profile.displayName,
+				usuario.gender = profile.gender,
+				usuario.provider = profile.provider,
+				usuario.hometown = profile._json.hometown,
+				usuario.location = profile._json.location
+			}
+			usuario.save();
+			passport.serializeUser(function(usuario, done) {
+			  done(null, usuario.id);
+			});
+
+			passport.deserializeUser(function(id, done) {
+			  Usuario.find({id: id}, function(err, usuario) {
+			    done(err, usuario);
+			  });
+			});
+
+			done(null, usuario);
+
+		});
+
+	}
+));
+
+
+/*App setup & init*/
+var express = require('express');
+var app = express();
+app.listen(4730);
+app.configure(function() {
+  app.use(express.static('public'));
+  app.use(express.cookieParser());
+  app.use(express.bodyParser());
+  app.use(express.session({ secret: 'tororoto' }));
+  app.use(passport.initialize());
+  app.use(passport.session());
+  app.use(app.router);
+});
+
+
+// Redirect the user to Facebook for authentication.  When complete,
+// Facebook will redirect the user back to the application at
+//     /auth/facebook/callback
+app.get('/auth/facebook', passport.authenticate('facebook'));
+
+// Facebook will redirect the user to this URL after approval.  Finish the
+// authentication process by attempting to obtain an access token.  If
+// access was granted, the user will be logged in.  Otherwise,
+// authentication has failed.
+app.get('/auth/facebook/callback', passport.authenticate('facebook', { successRedirect: '/auth/facebook/ok', failureRedirect: '/login' }));
+app.get('/auth/facebook/ok', function(req, res){
+	res.redirect('http://toro.ved/');
+});
+
+app.get('/login', function(req, res){
+	res.type('text/plain');
+	res.send('login route');
+});
 
 app.get('/ved', function(req, res){
 
