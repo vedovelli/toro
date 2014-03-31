34d57b765b6c3b4420cce8fa33091f77a03f05b2
Vedovelli <vedovelli@gmail.com>
2014-03-31 14:10:54 -0300

    alterado para buscar o candidato por slug ao invés de _id. Removido o array de objetos hardcoded na nodejs/app.js em favor da persistencia dos dados no MongoDB

--- js/router.js
+++ js/router.js
@@ -1,18 +1,18 @@
 Toro.Router.map(function() {
 	this.resource('ranking', function(){
-		this.resource('candidato', {path: '/candidato/:id'});
+		this.resource('candidato', {path: '/candidato/:slug'});
 	});
 	this.resource('sobre');
 });
 
 Toro.RankingRoute = Ember.Route.extend({
-  model: function(){
-	return Ember.$.getJSON(Toro.configs.webservice_uri + '/candidatos');
-  }
+	model: function(){
+		return Ember.$.getJSON(Toro.configs.webservice_uri + '/candidatos');
+	}
 });
 
 Toro.CandidatoRoute = Ember.Route.extend({
 	model: function(params){
-		return Ember.$.getJSON(Toro.configs.webservice_uri + '/candidato/'+params.id);
+		return Ember.$.getJSON(Toro.configs.webservice_uri + '/candidato/'+params.slug);
 	}
 });
--- nodejs/app.js
+++ nodejs/app.js
@@ -1,23 +1,60 @@
+
+/*App setup & init*/
 var express = require('express');
 var app = express();
-
 app.listen(4730);
 
+
+
+
+/*Database setup, init & models*/
+var Candidato;
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://127.0.0.1/toro');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function callback () {
+
+	/*Models*/
+	var candidatoSchema = mongoose.Schema({
+	    nome: String,
+	    sobrenome: String,
+	    slug: String
+	});
+
+	Candidato = mongoose.model('Candidato', candidatoSchema);
+
+});
+
+
+
+app.get('/ved', function(req, res){
+
+	Candidato.remove({ _id: '53399c47475f7dccc5a095f6' }, function(err, candidato){
+		var data = candidato[0];
+		res.json(data);
+	});
+
+
+});
+
+
+
+/*Main route*/
 app.get('/', function(req, res) {
   res.type('text/plain');
-  res.send('Toro®');
+  res.send('Toro');
 });
 
 
+/*Other routes*/
 app.get('/candidatos', function(req, res){
-	var candidatos = [
-		{id: 1,nome: 'Dilma', sobrenome: 'Roussef'},
-		{id: 2,nome: 'Aécio', sobrenome: 'Neves'},
-		{id: 3,nome: 'Eduardo', sobrenome: 'Campos'},
-	];
-	res.setHeader("Access-Control-Allow-Origin", "*");
-	res.json(candidatos);
+	var candidatos = Candidato.find(function(err, candidatos){
+		res.setHeader("Access-Control-Allow-Origin", "*");
+		res.json(candidatos);
+	});
 });
+
 
 app.get('/candidato/:id', function(req, res){
 	res.setHeader("Access-Control-Allow-Origin", "*");
